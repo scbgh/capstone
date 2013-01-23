468a5e862b010a6a2d22992fff8c08f3b715cab2
scbgh <sbroadhead@gmail.com>
2013-01-23 05:43:23 -0500

    Main window updated with shell of user interface

--- editor/src/mainwindow.cpp
+++ editor/src/mainwindow.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "mainwindow.h"
+#include "mapscene.h"
+#include "propertybrowser.h"
+#include <QtGui>
 
 //
 //
@@ -13,34 +16,131 @@ MainWindow::MainWindow()
     createToolBars();
     createStatusBar();
     createDockWindows();
+
+    scene = new MapScene(this);
+    view = new QGraphicsView(scene);
+
+    toolButtonGroup = new QButtonGroup(this);
+    QAbstractButton *selectButton = createToolButton("Select");
+    toolButtonGroup->addButton(selectButton, 0);
+    toolButtonGroup->addButton(createToolButton("Draw Polygon"), 1);
+    toolButtonGroup->addButton(createToolButton("Draw Circle"), 2);
+    selectButton->setChecked(true);
+
+    toolBoxLayout->addStretch();
+
+
+    setCentralWidget(view);
     setWindowTitle(tr("PGEditor"));
+    setUnifiedTitleAndToolBarOnMac(true);
+
+    resize(1280, 720);
+
+    readSettings();
+}
+
+//
+// Load an icon at three sizes, 16x16, 24x24, and 32x32.
+QIcon MainWindow::loadIcon(const QString& name)
+{
+    QIcon icon = QIcon(QString(":/icons16/") + name);
+    icon.addFile(QString(":/icons24/") + name);
+    icon.addFile(QString(":/icons32/") + name);
+    return icon;
+}
+
+//
+//
+QAbstractButton *MainWindow::createToolButton(const QString& name)
+{
+    QPushButton *button = new QPushButton(name);
+    toolBoxLayout->addWidget(button);
+    button->setCheckable(true);
+    return button;
 }
 
 //
 //
 void MainWindow::createActions()
 {
+    quitAct = new QAction(tr("&Quit"), this);
+    quitAct->setShortcuts(QKeySequence::Quit);
+    quitAct->setStatusTip(tr("Quit the application"));
+    connect(quitAct, SIGNAL(triggered()), this, SLOT(close()));
 }
 
 //
 //
 void MainWindow::createMenus()
 {
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(quitAct);
+
+    editMenu = menuBar()->addMenu(tr("&Edit"));
+
+    viewMenu = menuBar()->addMenu(tr("&View"));
 }
 
 //
 //
 void MainWindow::createToolBars()
 {
+    fileToolBar = addToolBar(tr("File"));
 }
 
 //
 //
 void MainWindow::createStatusBar()
 {
+    statusBar()->showMessage(tr("Ready"));
 }
 
+//
 void MainWindow::createDockWindows()
 {
+    QDockWidget *propertyDock = new QDockWidget(tr("Properties"), this);
+    propertyBrowser = new PropertyBrowser(propertyDock);
+    propertyDock->setWidget(propertyBrowser);
+    propertyDock->setObjectName("propertyDock");
+    addDockWidget(Qt::LeftDockWidgetArea, propertyDock);
+    viewMenu->addAction(propertyDock->toggleViewAction());
+
+    QDockWidget *toolBoxDock = new QDockWidget(tr("Toolbox"), this);
+    QWidget *toolBox = new QWidget(toolBoxDock);
+    toolBoxLayout = new QVBoxLayout(toolBox);
+    toolBox->setLayout(toolBoxLayout);
+    toolBoxDock->setWidget(toolBox);
+    toolBoxDock->setObjectName("toolBoxDock");
+    addDockWidget(Qt::LeftDockWidgetArea, toolBoxDock);
+    viewMenu->addAction(toolBoxDock->toggleViewAction());
 }
 
+//
+//
+void MainWindow::readSettings()
+{
+    QSettings settings;
+    settings.beginGroup("window");
+    if (settings.contains("geometry")) restoreGeometry(settings.value("geometry").toByteArray());
+    if (settings.contains("windowState")) restoreState(settings.value("windowState").toByteArray());
+    settings.endGroup();
+}
+
+//
+//
+void MainWindow::writeSettings()
+{
+    QSettings settings;
+    settings.beginGroup("window");
+    settings.setValue("geometry", saveGeometry());
+    settings.setValue("windowState", saveState());
+    settings.endGroup();
+}
+
+//
+//
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    writeSettings();
+    event->accept();
+}
--- editor/src/mainwindow.h
+++ editor/src/mainwindow.h
@@ -8,9 +8,15 @@
 
 #include <QMainWindow>
 
+class MapScene;
+class PropertyBrowser;
+class QAbstractButton;
 class QAction;
+class QButtonGroup;
+class QGraphicsView;
 class QMenu;
-
+class QToolBar;
+class QVBoxLayout;
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -18,12 +24,33 @@ class MainWindow : public QMainWindow {
 public:
     MainWindow();
 
+protected:
+    virtual void closeEvent(QCloseEvent *event);
+
 private:
+    QIcon loadIcon(const QString& name);
+    QAbstractButton *createToolButton(const QString& name);
+
     void createActions();
     void createMenus();
     void createToolBars();
     void createStatusBar();
     void createDockWindows();
+
+    void readSettings();
+    void writeSettings();
+
+    QAction *quitAct;
+
+    QGraphicsView *view;
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *viewMenu;
+    QToolBar *fileToolBar;
+    QVBoxLayout *toolBoxLayout;
+    QButtonGroup *toolButtonGroup;
+    MapScene *scene;
+    PropertyBrowser *propertyBrowser;
 };
 
 #endif
