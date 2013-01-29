8bfa70c98900188d604d6a921355999c62305977
scbgh <sbroadhead@gmail.com>
2013-01-29 17:56:52 -0500

    MapScene now drawing grid and background

--- editor/src/mapscene.cpp
+++ editor/src/mapscene.cpp
@@ -1,3 +1,105 @@
 //
 // mapscene.cpp
 //
+
+#include "mapscene.h"
+#include <cmath>
+#include <QtAlgorithms>
+#include <stdio.h>
+
+//
+//
+MapScene::MapScene(QGraphicsView *view, QObject *parent) :
+    QGraphicsScene(parent),
+    view_(view),
+    gridSize_(0.25),
+    showGrid_(true)
+{
+    addRect(0, 0, 1, 1, QPen(Qt::red));
+}
+
+//
+//
+void MapScene::drawBackground(QPainter *painter, const QRectF& rect)
+{
+    painter->fillRect(rect, Qt::black);
+}
+
+//
+//
+void MapScene::drawForeground(QPainter *painter, const QRectF& rect)
+{
+    // Draw the grid
+    painter->setWorldMatrixEnabled(true);
+
+    qreal left = qMax(rect.left() - (fmod(rect.left(), gridSize_)), 0.);
+    qreal top = qMax(rect.top() - (fmod(rect.top(), gridSize_)), 0.);
+    qreal right = qMin(rect.right(), sceneRect().right());
+    qreal bottom = qMin(rect.bottom(), sceneRect().bottom());
+
+    // Don't render the grid if it's too dense
+    if (rect.width() / gridSize_ > 1024) {
+        return;
+    }
+
+    QVarLengthArray<QLineF, 1024> linesX;
+    for (qreal x = left; x <= right; x += gridSize_)
+        linesX.append(QLineF(x, top, x, bottom));
+
+    QVarLengthArray<QLineF, 1024> linesY;
+    for (qreal y = top; y <= bottom; y += gridSize_)
+        linesY.append(QLineF(left, y, right, y));
+
+    if (showGrid_) {
+        painter->setPen(QColor(255, 255, 255, 64)); // TODO: customizable colour?
+        painter->drawLines(linesX.data(), linesX.size());
+        painter->drawLines(linesY.data(), linesY.size());
+    }
+}
+
+//
+//
+void MapScene::setMap(GameMap& map)
+{
+    map_ = QSharedPointer<GameMap>(&map);
+    sync();
+}
+
+//
+//
+void MapScene::sync()
+{
+    clear();
+    if (!map_) {
+        return;
+    }
+
+    for (const auto& shape : map_->shapes) { addShape(*shape); }
+    for (const auto& body : map_->bodies) { addBody(*body); }
+    for (const auto& fixture : map_->fixtures) { addFixture(*fixture); }
+    for (const auto& joint : map_->joints) { addJoint(*joint); }
+}
+
+//
+//
+void MapScene::addShape(const Shape& shape)
+{
+}
+
+//
+//
+void MapScene::addBody(const Body& body)
+{
+}
+
+//
+//
+void MapScene::addFixture(const Fixture& fixture)
+{
+}
+
+//
+//
+void MapScene::addJoint(const Joint& joint)
+{
+}
--- editor/src/mapscene.h
+++ editor/src/mapscene.h
@@ -6,16 +6,42 @@
 #ifndef _MAPSCENE_H_
 #define _MAPSCENE_H_
 
+#include "mapdata.h"
+#include <memory>
 #include <QGraphicsScene>
 
 class MapScene : public QGraphicsScene {
     Q_OBJECT
 
 public:
-    MapScene(QObject *parent = 0) :
-        QGraphicsScene(parent)
-    {
-    }
+    MapScene(QGraphicsView *view, QObject *parent = 0);
+
+    const GameMap& map() const { return *map_; }
+    void setMap(GameMap& map_);
+
+    float gridSize() const { return gridSize_; }
+    void setGridSize(float gridSize) { gridSize_ = gridSize; update(); }
+
+    bool showGrid() const { return showGrid_; }
+    void setShowGrid(bool showGrid) { showGrid_ = showGrid; update(); }
+
+protected:
+    virtual void drawBackground(QPainter *painter, const QRectF& rect);
+    virtual void drawForeground(QPainter *painter, const QRectF& rect);
+
+private:
+    // Refresh the items in the scene to match the map
+    void sync();
+
+    void addShape(const Shape& shape);
+    void addBody(const Body& body);
+    void addFixture(const Fixture& fixture);
+    void addJoint(const Joint& joint);
+
+    QSharedPointer<GameMap> map_;
+    float gridSize_;
+    bool showGrid_;
+    QGraphicsView *view_;
 };
 
 #endif
