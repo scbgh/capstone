a1b2727ec5c57c02b68ff7c5a1dde1b00c223100
scbgh <sbroadhead@gmail.com>
2013-02-03 14:52:28 -0500

    Added CircleShapeItem

---
+++ editor/src/sceneitems/circleshapeitem.cpp
@@ -0,0 +1,78 @@
+//
+//  circleshapeitem.cpp
+//
+
+#include <QtGui>
+#include "mapdata.h"
+#include "mapscene.h"
+#include "circleshapeitem.h"
+
+//
+//
+CircleShapeItem::CircleShapeItem(QSharedPointer<CircleShape> shape, QGraphicsItem *parent, QGraphicsScene *scene) :
+    ShapeItem(shape),
+    QGraphicsEllipseItem(parent, scene),
+    complete_(false)
+{
+    setFlag(QGraphicsItem::ItemIsMovable, true);
+    setFlag(QGraphicsItem::ItemIsSelectable, true);
+    setFlag(QGraphicsItem::ItemSendsGeometryChanges, true);
+}
+
+//
+//
+void CircleShapeItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    QBrush drawBrush = brush();
+    QPen drawPen = pen();
+
+    if (isSelected()) {
+        drawBrush = QColor(128, 255, 128, 128);
+        drawPen = QColor(128, 255, 128);
+    }
+
+    painter->setBrush(drawBrush);
+    painter->setPen(drawPen);
+    if (complete_) {
+        painter->drawEllipse(boundingRect());
+    } else {
+        painter->setBrush(Qt::transparent);
+        painter->drawEllipse(boundingRect());
+    }
+}
+
+//
+//
+void CircleShapeItem::sync()
+{
+    QSharedPointer<CircleShape> circle = qSharedPointerCast<CircleShape>(shape_);
+    setRect(-circle->radius, -circle->radius, 2*circle->radius, 2*circle->radius);
+    setPos(circle->position);
+}
+
+//
+// Commit changes to this Shape item to the underlying data object
+void CircleShapeItem::commit()
+{
+    QSharedPointer<CircleShape> circle = qSharedPointerCast<CircleShape>(shape_);
+    circle->radius = rect().width() / 2;
+    circle->position = pos();
+}
+
+//
+//
+QVariant CircleShapeItem::itemChange(GraphicsItemChange change, const QVariant& value)
+{
+    if (change == ItemPositionChange && mapScene()) {
+        QPointF newPos = mapScene()->snapPoint(value.toPointF());
+        return newPos;
+    }
+    return QGraphicsItem::itemChange(change, value);
+}
+
+//
+//
+QAbstractGraphicsShapeItem *CircleShapeItem::innerShape() const 
+{
+    return (QAbstractGraphicsShapeItem *)this;
+}
---
+++ editor/src/sceneitems/circleshapeitem.h
@@ -0,0 +1,32 @@
+//
+//  circleshapeitem.h
+//  A QGraphicsScene shape representing a circle in the map.
+//
+
+#ifndef _CIRCLESHAPEITEM_H_
+#define _CIRCLESHAPEITEM_H_
+
+#include <QGraphicsEllipseItem>
+#include "shapeitem.h"
+
+struct CircleShape;
+
+class CircleShapeItem : public QGraphicsEllipseItem, public ShapeItem {  
+public:
+    explicit CircleShapeItem(QSharedPointer<CircleShape> shape, QGraphicsItem *parent = 0, QGraphicsScene *scene = 0);  
+
+    bool complete() const { return complete_; }
+    void setComplete(bool complete) { complete_ = complete; }
+
+    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+    virtual void sync();
+    virtual void commit();
+    virtual QVariant itemChange(GraphicsItemChange change, const QVariant& value);
+    virtual QAbstractGraphicsShapeItem *innerShape() const;
+
+    virtual int type() const { return kCircleShapeItem; }
+private:
+    bool complete_; // complete polygon, should be filled
+};
+
+#endif
