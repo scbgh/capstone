864b0b5a25dda9ffa9909840465332b41a505cdc
scbgh <sbroadhead@gmail.com>
2013-02-19 16:53:27 -0500

    Updated VertexItem to have commit and sync functions as parameters

--- editor/src/sceneitems/vertexitem.cpp
+++ editor/src/sceneitems/vertexitem.cpp
@@ -2,15 +2,62 @@
 //  vertexitem.cpp
 //
 
+#include "mapscene.h"
 #include "vertexitem.h"
 
 //
 //
-VertexItem::VertexItem(int id, QGraphicsItem *parent, QGraphicsScene *scene) :
-    QGraphicsPolygonItem(parent, scene),
-    id_(id)
+VertexItem::VertexItem(std::function<QPointF()> syncFunc, std::function<void(QPointF)> commitFunc,
+        QGraphicsItem *parent, QGraphicsScene *scene) :
+    QGraphicsEllipseItem(parent, scene),
+    syncFunc_(syncFunc),
+    commitFunc_(commitFunc)
 {
     setFlag(QGraphicsItem::ItemIsMovable, true);
     setFlag(QGraphicsItem::ItemIsSelectable, true);
     setFlag(QGraphicsItem::ItemSendsGeometryChanges, true);
+}
+
+//
+void VertexItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    QBrush drawBrush = brush();
+    QPen drawPen = pen();
+
+    if (isSelected()) {
+        drawBrush = QColor(255, 255, 255, 128);
+        drawPen = QColor(255, 255, 255);
+    }
+
+    painter->setBrush(drawBrush);
+    painter->setPen(drawPen);
+    painter->drawEllipse(boundingRect());
+}
+
+//
+//
+void VertexItem::sync()
+{
+    setPos(syncFunc_());
+}
+
+//
+// Commit changes to this Shape item to the underlying data object
+void VertexItem::commit()
+{
+    commitFunc_(scenePos());
+}
+
+//
+//
+QVariant VertexItem::itemChange(GraphicsItemChange change, const QVariant& value)
+{
+    MapScene *mapScene = (MapScene *)scene();
+    if (change == ItemPositionChange && mapScene) {
+        QPointF newPos = mapScene->snapPoint(value.toPointF());
+        return newPos;
+    } else if (change == ItemPositionHasChanged) {
+        commit();
+    }
+    return QGraphicsItem::itemChange(change, value);
 }
--- editor/src/sceneitems/vertexitem.h
+++ editor/src/sceneitems/vertexitem.h
@@ -6,18 +6,23 @@
 #ifndef _VERTEXITEM_H_
 #define _VERTEXITEM_H_
 
-#include <QGraphicsItem>
+#include "sceneitem.h"
+#include <functional>
+#include <QGraphicsEllipseItem>
 
-class VertexItem : QGraphicsItem {
+class VertexItem : public QGraphicsEllipseItem {
 public:
-    explicit VertexItem(int id, QGraphicsItem *parent = 0, QGraphicsScene *scene = 0);
+    explicit VertexItem(std::function<QPointF()> syncFunc, std::function<void(QPointF)> commitFunc,
+        QGraphicsItem *parent = 0, QGraphicsScene *scene = 0);
 
-public slots:
-    void vertexMoved(int id);
-    void vertexDeleted(int id);
+    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
+    void sync();
+    void commit();
+    virtual QVariant itemChange(GraphicsItemChange change, const QVariant& value);
 
 private:
-    int id_;
+    std::function<QPointF()> syncFunc_;
+    std::function<void(QPointF)> commitFunc_;
 };
 
 #endif
