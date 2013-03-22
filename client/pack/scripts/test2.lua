bbe4a6fa1f3e24da2a2ca63ec26862df579a8176
scbgh <sbroadhead@gmail.com>
2013-03-22 18:22:00 -0400

    Updated character art and test levels

--- client/pack/scripts/test2.lua
+++ client/pack/scripts/test2.lua
@@ -3,22 +3,13 @@ function init(s)
 end
 
 function key_down(s, key)
- joint = tolua.cast(s:joint("wheeljoint"), "b2RevoluteJoint")
-    if key == "z" then
-        joint:SetMotorSpeed(-150)
-        joint:EnableMotor(true)
-    elseif key == "x" then
-        joint:SetMotorSpeed(150)
-        joint:EnableMotor(true)
-    end
 end
 
 function key_up(s, key)
-    joint = s:joint("wheeljoint")
-    joint:EnableMotor(false)
 end
 
 function begin_contact(s, contact)
+    print(contact_velocity(contact))
 end
 
 function end_contact(s, contact)
